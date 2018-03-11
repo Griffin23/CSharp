e68e129c3a167fb1ac6a04432a91724a1d5d15b5
Duke <838884524@qq.com>
2018-03-11 13:10:38 +0800

    feat: get enum description

---
+++ 获取enum的description/EnumExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPro.Utils.Enum
+{
+    public static class EnumExtension
+    {
+        public static string ToDescription<T>(this T myEnum)
+        {
+            Type type = typeof(T);
+            FieldInfo info = type.GetField(myEnum.ToString());
+            DescriptionAttribute descriptionAttribute = info.GetCustomAttributes(typeof(DescriptionAttribute), true)[0] as DescriptionAttribute;
+            if (descriptionAttribute != null)
+                return descriptionAttribute.Description;
+            else
+                return type.ToString();
+        }
+    }
+}
---
+++ 获取enum的description/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MyPro.Utils.Enum;
+using System.ComponentModel;
+
+namespace MyPro
+{
+    class Program
+    {
+        private enum colors {
+            [Description("red color")]
+            red,
+            [Description("yellow color")]
+            yellow,
+            [Description("blue color")]
+            blue
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(colors.red.ToDescription());
+            string s = "1111";
+            Console.WriteLine(s.ToDescription());
+        }
+    }
+} 
