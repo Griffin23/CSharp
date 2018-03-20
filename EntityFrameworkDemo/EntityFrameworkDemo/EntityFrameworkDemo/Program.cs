5acb775b4585c2e253a4922b191454caf2d9456a
Duke <838884524@qq.com>
2018-03-20 08:59:23 +0800

    feat: EntityFramework Demo

---
+++ EntityFrameworkDemo/EntityFrameworkDemo/EntityFrameworkDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Model;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resultText = string.Empty;
+            var en = new Entities();
+            var query = from e in en.MYTABLE
+                        where e.ID == 1
+                        select e;
+            if (query.Count() == 0)
+                resultText = "No result!";
+            var result = query.First();
+            resultText = result.USERNAME + " " + result.SEX;
+            Console.WriteLine(resultText);
+        }
+    }
+}
