0dc617a3409d702f080f480627e8be64470f68c9
Duke <838884524@qq.com>
2018-04-02 15:06:48 +0800

    feat: CommonUtil: Log4net + HttpRuntimeCache

---
+++ CommonUtil/CommonUtil/CommonUtil/Util/HttpRuntimeCacheHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CommonUtil.Util
+{
+    public class HttpRuntimeCacheHelper
+    {
+        public static void SetAbsoluteExpirationCache(string key, object value, double milliSeconds)
+        {
+            HttpRuntime.Cache.Add(key, value, null, DateTime.UtcNow.AddMilliseconds(milliSeconds),
+                System.Web.Caching.Cache.NoSlidingExpiration,
+                System.Web.Caching.CacheItemPriority.Normal, null);
+        }
+
+        public static void SetSlidingExpirationCache(string key, object value, double milliSeconds)
+        {
+            HttpRuntime.Cache.Add(key, value, null, System.Web.Caching.Cache.NoAbsoluteExpiration,
+                TimeSpan.FromMilliseconds(milliSeconds),
+                System.Web.Caching.CacheItemPriority.Normal, null);
+        }
+
+        public static object GetCache(string key)
+        {
+            return HttpRuntime.Cache.Get(key);
+        }
+
+        public static void removeCache(string key)
+        {
+            HttpRuntime.Cache.Remove(key);
+        }
+    }
+}
---
+++ CommonUtil/CommonUtil/Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommonUtil.Util;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test
+            Log4netHelper.Error("error test");
+            Log4netHelper.Info("info test");
+        }
+    }
+}
