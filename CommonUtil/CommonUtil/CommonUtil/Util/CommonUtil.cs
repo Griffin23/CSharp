5ebd706dbd49dbc31b87589c15ce002a509620e7
Duke <838884524@qq.com>
2018-06-25 10:12:52 +0800

    feat: .NET 获取客户端请求IP

---
+++ CommonUtil/CommonUtil/CommonUtil/Util/CommonUtil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CommonUtil.Util
+{
+    public class CommonUtil
+    {
+        // 获取客户端IP
+        public static string GetClientIp()
+        {
+            string ip = string.Empty;
+            if (HttpContext.Current.Request.ServerVariables["HTTP_VIA"] != null) // using proxy
+            {
+                ip = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"].ToString();  // Return real client IP.
+
+            }
+            else // not using proxy or can't get the Client IP
+            {
+                ip = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"].ToString(); // While it can't get the Client IP, it will return proxy IP.
+            }
+            return ip;
+        }
+    }
+}
