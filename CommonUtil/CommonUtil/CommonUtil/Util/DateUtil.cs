15fcec8ea1ebbe9163880b6f8400936467c542f7
howrudoing <howru@doing.com>
2019-08-30 17:21:38 +0800

    feat: 获取当前时间戳

---
+++ CommonUtil/CommonUtil/CommonUtil/Util/DateUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonUtil.Util
+{
+    public class DateUtil
+    {
+        /// <summary>
+        /// 获取当前时间戳（秒）
+        /// </summary>
+        /// <returns></returns>
+        public static int GetCurrentTimestamp()
+        {
+            return (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+    }
+}
